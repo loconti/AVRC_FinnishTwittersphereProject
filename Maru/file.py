e02437b1bd67cc17cccb34864043f080fa2b5a2e
mariantoniettaltierima-sketch <mariantoniettaltieri.ma@gmail.com>
2026-06-01 16:37:50 +0200

    barplot

--- Maru/file.py
+++ Maru/file.py
@@ -3,3 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # misure di centralità
+DIR = '../Data/'
+def load_graph(filename='climate_19.graphml') -> ig.Graph:
+    return ig.Graph.Read_GraphML(DIR + filename)
+
+G = load_graph()
+
+degrees = G.degree()
+bins = 7
+fig, ax = plt.subplots(figsize=(12,6), layout='standard')
+ax.hist(degrees, bins=bins, histtype='stepfilled', color='red', alpha=0.60, edgecolor='red', linewidth=2.0)
+
