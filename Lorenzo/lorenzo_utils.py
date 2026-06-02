4fc8fdb975a3a52a0d2d426930cb6c31f78c98c8
loconti <lo.conti00@outlook.com>
2026-06-02 16:48:28 +0200

    Plot distribuzioni di centralità

--- Lorenzo/lorenzo_utils.py
+++ Lorenzo/lorenzo_utils.py
@@ -38,3 +38,11 @@ def load_all_centralities(G: ig.Graph, dumpfile: str=""):
         G.write_graphml(dumpfile)
 
     return centralities
+
+def compute_ccdf(data: np.ndarray) -> tuple[np.ndarray,np.ndarray]:
+    N = len(data)
+    x_values = np.linspace(0,data.max(),5000)
+    ccdf = np.array([np.sum(data>x) / N for x in x_values])
+    return x_values, ccdf
+
+cmap = plt.get_cmap('tab10')
